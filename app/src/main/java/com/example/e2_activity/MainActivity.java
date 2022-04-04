3ff5c045c02e1eae6304f7c8fad76b368f4b0d04
Pablo <73362636+PabloGO4@users.noreply.github.com>
2022-04-04 13:40:17 +0200

    versión 0.3

--- app/src/main/java/com/example/e2_activity/MainActivity.java
+++ app/src/main/java/com/example/e2_activity/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.e2_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void OnClickBtn (View view){
+        Intent i = new Intent(this,pan);
+        Switch
     }
 }
